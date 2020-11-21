a40dbe9c1c54ddefdc8c2318d5bda69b9c1c59e1
Jacob Libsman <40018663+Libsmj@users.noreply.github.com>
2020-11-21 05:07:23 -0500

    Fixes to matlab code

--- project2.m
+++ project2.m
@@ -1,28 +1,34 @@
-%max_cut_sdp.m
-clear;clc;
-n=10; %number of nodes
-A=zeros(n,n); %adjacency matrix
-edges=[[1,2];[2,3];[3,4];[4,5];[5,1];...
-       [1,6];[2,7];[3,8];[4,9];[5,10];...
-       [6,8];[6,9];[7,9];[7,10];[8,10]];
-A(sub2ind(size(A),edges(:,1),edges(:,2)))=1;
-A=A+A';
-
-% %% Set up a random graph
+%% max_cut_sdp.m
 % clear;clc;
-% n = 50;
-% p = 0.4;
-% A = triu(rand(n)<p);
-% A = A+A';
+% n=10; %number of nodes
+% A=zeros(n,n); %adjacency matrix
+% edges=[[1,2];[2,3];[3,4];[4,5];[5,1];...
+%        [1,6];[2,7];[3,8];[4,9];[5,10];...
+%        [6,8];[6,9];[7,9];[7,10];[8,10]];
+% A(sub2ind(size(A),edges(:,1),edges(:,2)))=1;
+% A=A+A';
 
-%% CVX
-cvx_begin sdp
+%% Set up a random graph
+n = 50;
+
+p = 0.4;
+A = rand(n) < p;
+A = triu(A) + triu(A,1)';
+A = A - diag(diag(A));
+
+
+cvx_begin sdp quiet
     variable X(n,n) symmetric
-    minimize(-trace(A*(ones(n,n)-X))/4)
+    minimize(-trace( A* (ones(n,n)-X ))/4)
     subject to
     diag(X)==ones(n,1)
     X>=0
     X == semidefinite(n);
 cvx_end
 
-W=sign(X*(rand(size(X,1),1)-.5));
+V = chol(X); %Factor
+w = rand(n,1); %Random vector
+y = sign(V*w); %Partition data
+
+%Output size of cut
+fprintf("Size of cut: %d\n",trace(A * (1 - y*y'))/4);
