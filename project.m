99aa25af853b394950f49c3e27954a70a46906f3
Jacob Libsman <40018663+Libsmj@users.noreply.github.com>
2020-11-29 19:31:55 -0500

    Full maxcut python solver

--- project.m
+++ project.m
@@ -1,34 +1,26 @@
 %% Set up a random graph
-n = 100;
+n = 20;
+rng(2)
 
-% a = zeros(125-1,1);
-% for n = 2:125
+p = 0.4;
+A = rand(n) < p;
+A = triu(A) + triu(A,1)';
+A = A - diag(diag(A));
+disp(A)
 
-    p = 0.4;
-    A = rand(n) < p;
-    A = triu(A) + triu(A,1)';
-    A = A - diag(diag(A));
-
-    % Use CVX
-    tic
-    cvx_begin quiet
-        variable X(n,n) symmetric
-        minimize trace(A*X)
-            diag(X) == ones(n,1);
-            X == semidefinite(n);
-    cvx_end
-%     a(n-1) = toc;
-% end
-% hold on
-% xlim([2 125])
-% plot(2:125, a)
-% ylabel("cpu time");
-% xlabel("n");
-% hold off
+% Use CVX
+cvx_begin quiet
+    variable X(n,n) symmetric
+    minimize trace(A*X)
+        diag(X) == ones(n,1);
+        X == semidefinite(n);
+cvx_end
 
 %%
 U = chol(X);
 r = mvnrnd(zeros(n,1),diag(ones(n,1)))';
 y = sign(U*r);
-cut = (sum(A(:)) - y'*A*y)/4
-SDP_opt = (sum(A(:)) - trace(A*X))/4;
+cut = (sum(A(:)) - y'*A*y)/4;
+disp(cut)
+
+% SDP_opt = (sum(A(:)) - trace(A*X))/4;
--- sdp_example.py
+++ sdp_example.py
@@ -2,22 +2,27 @@ import cvxpy as cp
 import numpy as np
 
 # Generate a random SDP.
-n = 3
-np.random.seed(2)
-C = np.random.randn(n, n)
+n = 20
+p = 0.4
+A = (np.random.rand(n, n) < p).astype(int)
+A = np.triu(A) + np.triu(A).T
+A = A - np.diag(np.diag(A))
 
 # Define and solve the CVXPY problem.
-# Create a symmetric matrix variable.
+# Create a symmetric matrix variable and constraints
 X = cp.Variable((n,n), symmetric=True)
-# The operator >> denotes matrix inequality.
 constraints = [X >> 0]
 constraints += [
     cp.diag(X) == 1
 ]
-prob = cp.Problem(cp.Minimize(cp.trace(C @ X)), constraints)
+prob = cp.Problem(cp.Minimize(cp.trace(A @ X)), constraints)
 prob.solve(solver='CVXOPT')
 
-# Print result.
-print("The optimal value is", prob.value)
-print("A solution X is")
-print(X.value)
+# Solve for the maximum cut
+U = np.linalg.cholesky(X.value)
+r = np.random.normal(0, 1, n)
+y = np.sign(U @ r)
+
+# Calculate the cut
+cut = (np.sum(A) - y.T@A@y)/4
+print(cut)
